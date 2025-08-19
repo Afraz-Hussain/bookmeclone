31b65d77fe50540a30bb7a3b9c51416a6bd4c41c
afraz <farazishah14@gmail.com>
2025-08-19 16:07:10 +0500

    trending dest designed

--- src/pages/home/TrendingDest.jsx
+++ src/pages/home/TrendingDest.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 
 const TrendingDest = () => {
   return (
-    <div className="px-6 mt-4">
-      <h2 className="text-2xl font-bold mt-3">Trending destinations</h2>
-      <p>Most popular choices for travelers from Pakistan</p>
+    <div className="px-6 mt-4 mb-40">
+      <div>
+        <h2 className="text-2xl font-bold mt-3">Trending destinations</h2>
+        <p className="text-gray-600">
+          Most popular choices for travelers from Pakistan
+        </p>
+      </div>
 
+      {/* upper boxes having 2 cities lhr and isl */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
+        {/* Lahore box */}
+        <div
+          className="h-60 rounded-xl bg-cover bg-center flex items-start p-3 
+          text-white font-bold text-lg pt-4 cursor-pointer hover:border border-yellow-400"
+          style={{ backgroundImage: "url('pics/lahore.jpg')" }}
+        >
+          <div className="flex items-center">
+            Lahore
+            <img src="pics/Pk.png" className="w-6 h-4 ml-2 mt-1" alt="PK flag" />
+          </div>
+        </div>
+
+        {/* Islamabad box */}
+        <div
+          className="h-60 rounded-xl bg-cover bg-center flex items-start p-3 
+          text-white font-bold text-lg pt-4 cursor-pointer hover:border border-yellow-400"
+          style={{ backgroundImage: "url('pics/isl.jpg')" }}
+        >
+          <div className="flex items-center">
+            Islamabad
+            <img src="pics/Pk.png" className="w-6 h-4 ml-2 mt-1" alt="PK flag" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
